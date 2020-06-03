c8460ad17b8d07ca91af76af6dab5948597aa56c
donaldcrane <donaldagbakoba@gmail.com>
2020-06-03 01:12:07 +0100

    Updates

---
+++ precache-manifest.3a264486559cbdad750b8d1b8d335cf2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5ebfea448794e096d4aada9a8b3059",
+    "url": "/celeb/index.html"
+  },
+  {
+    "revision": "bad41a57a23e2ffe19ef",
+    "url": "/celeb/static/css/main.a01ef009.chunk.css"
+  },
+  {
+    "revision": "4e865a6565e68a89f971",
+    "url": "/celeb/static/js/2.3cd23d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/celeb/static/js/2.3cd23d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad41a57a23e2ffe19ef",
+    "url": "/celeb/static/js/main.ba4c8376.chunk.js"
+  },
+  {
+    "revision": "47fe1d02f425eec000d2",
+    "url": "/celeb/static/js/runtime-main.3fee21d1.js"
+  },
+  {
+    "revision": "ee54e0d4a6217a8fabc56e1b64e21643",
+    "url": "/celeb/static/media/2face.ee54e0d4.jpg"
+  },
+  {
+    "revision": "1b257aa8d1100f8c11964383cbc196b3",
+    "url": "/celeb/static/media/burna.1b257aa8.jpg"
+  },
+  {
+    "revision": "dd8933d1ad73124e5025ac2cc7cbf2f1",
+    "url": "/celeb/static/media/davido.dd8933d1.jpg"
+  },
+  {
+    "revision": "f1349efc1462e59ec917de69640b7967",
+    "url": "/celeb/static/media/dremo.f1349efc.jpg"
+  },
+  {
+    "revision": "88fae98a23a34162076e0c9fb4e0a38a",
+    "url": "/celeb/static/media/erriga.88fae98a.jpg"
+  },
+  {
+    "revision": "c6a10a69711781206d91ad5510ab0b54",
+    "url": "/celeb/static/media/marley.c6a10a69.jpg"
+  },
+  {
+    "revision": "16482d0bc9bef6a7c07ab6b5292c3a02",
+    "url": "/celeb/static/media/mayorkun.16482d0b.jpg"
+  },
+  {
+    "revision": "e20e58f5ee30cc5e0fc1bb548bd77c3f",
+    "url": "/celeb/static/media/olamide.e20e58f5.jpg"
+  },
+  {
+    "revision": "b7acca154bc09369ab8e735623aa9381",
+    "url": "/celeb/static/media/wizkid.b7acca15.jpg"
+  },
+  {
+    "revision": "d9ffb6a9357b7f59ec8819e8bb1a6475",
+    "url": "/celeb/static/media/zlatan.d9ffb6a9.jpg"
+  }
+]);
